ab21c7d3da96324accb9226844c6a8298a60261f
Boris Sustavov <sbv91@mail.ru>
2023-02-14 23:37:10 +0300

    сохранение после исправлений замечаний ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,43 @@
-const editPopup = document.querySelector('.editpopup');
+const editPopup = document.querySelector('.popup');
 const addButton = document.querySelector('.profile__edite-button');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-const editPopupName = document.querySelector('.editpopup__name');
-const editPopupDescription = document.querySelector('.editpopup__description');
-const editPopupSave = document.querySelector('.editpopup__save');
-const like = document.querySelectorAll('.element__like');
-const editPopupClose = document.querySelector('.editpopup__close');
+const editPopupName = document.querySelector('.popup__input-text_legend_name');
+const editPopupDescription = document.querySelector('.popup__input-text_legend_description');
+const editPopupClose = document.querySelector('.popup__close');
+const formEdite = document.querySelector('.popup__form');
+
+function close(namePopup) {
+  namePopup.classList.remove('popup_opened');
+}
+function open(namePopup) {
+  namePopup.classList.add('popup_opened');
+}
 
 /*закрыть popUP*/
 function closePopup(evt) {
   evt.preventDefault();
-  editPopup.classList.remove('editpopup_opened');
+  close(editPopup);
   editPopupName.value = '';
   editPopupDescription.value = '';
 }
-editPopupClose.addEventListener('click', closePopup);
 
-/*открыть popUP*/
+/*открыть popUP */
 function openPopup() {
-  editPopup.classList.add('editpopup_opened');
-  console.log(editPopup.classList);
+  open(editPopup);
   editPopupName.value = profileName.textContent;
   editPopupDescription.value = profileDescription.textContent;
 }
-addButton.addEventListener('click', openPopup);
 
 /* сохранить изменения и закрыть popUp*/
 function savePopup(evt) {
   evt.preventDefault();
   profileName.textContent = editPopupName.value;
   profileDescription.textContent = editPopupDescription.value;
-  editPopup.classList.remove('editpopup_opened');
+  close(editPopup);
 }
-editPopupSave.addEventListener('click', savePopup);
 
-/* Поставить/убрать LIKE*/
-/*перебор массива всех LIKEов*/
-like.forEach(element =>
-  /*каждому элементу добавить по клику добавить функцию смены сердечка*/
-  element.addEventListener('click', function () {
-    element.classList.toggle('element__like_active');
-  })
-);
+editPopupClose.addEventListener('click', closePopup);
+addButton.addEventListener('click', openPopup);
+//Клик по кнопке "Сохранить" необходимо удалить.Используйте событие submit у формы OK
+formEdite.addEventListener('submit', savePopup);
